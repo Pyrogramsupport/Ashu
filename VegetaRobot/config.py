351c3051a2e5b59fd25663747ed1f58f7b498816
𝐀𝐊𝐈𝐓𝐎 <samarkoli790@gmail.com>
2024-08-21 19:16:37 +0530

    Update config.py

--- VegetaRobot/config.py
+++ VegetaRobot/config.py
@@ -1,44 +1,39 @@
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
-import json #by ctzfamioy
+import json
 import os
 
-ARQ_API_URL = "https://arq.hamker.in"
-ARQ_API_KEY =  "KGTHQB-IKDNAI-XXFCND-VDZFQI-ARQ"
 
 def get_user_list(config, key):
     with open('{}/VegetaRobot/{}'.format(os.getcwd(), config),
               'r') as json_file:
         return json.load(json_file)[key]
 
-
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 class Config(object):
     LOGGER = True
     # REQUIRED
-    #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 12345   # integer value, dont use ""
-    API_HASH = "awew"
-    TOKEN = "wow"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    SQLALCHEMY_DATABASE_URI = "" #Use Your ElephantSQL
-    OWNER_ID = 1491497760  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "nandhaxd"
-    SUPPORT_CHAT = 'vegetasupport'  #Your own group for support, do not add the @
-    UPDATES_CHANNEL = 'vegetaUpdates' #Your own channel for Updates of bot, Do not add @
-    JOIN_LOGGER = -1001543354286  #Prints any new group the bot is added to, prints just the name and ID.
-    REM_BG_API_KEY = "dxsh728mZMDmj4ijSZCNPZig"
-    EVENT_LOGS = -1001543354286  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    API_ID = 29764570
+    API_HASH = "7676192e491329a1b859b2184e5d7823"
+    TOKEN = "7048029832:AAHtX9gs9kuNjqhhBUioueMbYSGGoh4ZKLk"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 6411059657
+    OWNER_USERNAME = "ELITE_GANESH"
+    SUPPORT_CHAT = 'E_H_Club'  #Own group for support, do not added the @
+    JOIN_LOGGER = -1001801945066  #Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = -1001801945066  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+
+    #RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = 'postgres://rkffvakt:o7mkbX8hS7h5IPhMyPHSteGaWvuDYOp-@surus.db.elephantsql.com/rkffvakt'  # needed for any database modules
     LOAD = []
     NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
-    WEBHOOK = None
+    WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key -
-    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
-    BOT_ID = "2128359921"
-    
-    DRAGONS = get_user_list('elevated_users.json', 'sudos')
+    SPAMWATCH_API = "QzhnoxpyhMXwSdpLMk77NUE8OPUJSMAsDMI088IHlPJS~_NenKRX5wbtAyCbCGZc"  
+    SPAMWATCH_SUPPORT_CHAT = "@E_H_Support"
 
+    #OPTIONAL
+    ##List of id's -  (not usernames) for users which have sudo access to the bot.
+    DRAGONS = get_user_list('elevated_users.json', 'sudos')
+    ##List of id's - (not usernames) for developers who will have the same perms as the owner
     DEV_USERS = get_user_list('elevated_users.json', 'devs')
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     DEMONS = get_user_list('elevated_users.json', 'supports')
@@ -48,7 +43,15 @@ class Config(object):
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
+    DEL_CMDS = True  #Delete commands that users dont have access to, like delete /ban if a non admin uses it.
+    STRICT_GBAN = True
     WORKERS = 8  # Number of subthreads to use. Set as number of threads your processor uses
+    BAN_STICKER = 'CAACAgUAAxkBAAIk8Ga7ZiCCRkhuBsT-cwAB09fHpddeWgAC8Q8AAhP7aVdcDFV00iDG_DQE'  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
+    CASH_API_KEY = 'H1HOGR3MMV2XWIF7' 
+    TIME_API_KEY = 'awoo'
+    WALL_API = 'awoo'  
+    AI_API_KEY = 'awoo'
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
 
